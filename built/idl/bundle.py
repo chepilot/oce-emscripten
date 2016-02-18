6d3a05a7bb253be0f8d6f14a3f7ec5cc83aaf9cf
chepilot <liruizenus@gmail.com>
2016-02-18 15:05:01 +0800

    gh-pages setup

--- built/idl/bundle.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-
-bundle = open(sys.argv[1], 'a')
-bundle.write(open('glue.js', 'r').read())
-bundle.write('''
-this['oce'] = Module; /* With or without a closure, the proper usage is oce.* */
-''')
-bundle.close()
-
